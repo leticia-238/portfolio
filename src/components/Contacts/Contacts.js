9a356c10263d562df66f6c6fabc44b824630a6da
leticia-238 <leticiaoliveiraandrade.9@gmail.com>
2022-10-14 13:56:43 -0300

    feat: adiciona icons no perfil

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,19 +1,19 @@
 import { Contact, ContactBox, ContactIcon } from './styles';
+import EmailIcon from './icons/envelope.png';
+import GithubIcon from './icons/github-logo.png';
+import LinkedinIcon from './icons/linkedin-logo.png';
 
 function Contacts() {
   return (
     <ContactBox>
       <Contact href="https://github.com/leticia-238" target="_blank">
-        <ContactIcon src="./" alt="icon" />
-        github
+        <ContactIcon src={ GithubIcon } alt="github" />
       </Contact>
       <Contact href="https://www.linkedin.com/in/leticiaoliveiraandrade" target="_blank">
-        <ContactIcon src="./" alt="icon" />
-        linkedin
+        <ContactIcon src={ LinkedinIcon } alt="linkedin" />
       </Contact>
       <Contact href="mailto:leticiaoliveiraandrade@gmail.com">
-        <ContactIcon src="./" alt="icon" />
-        email
+        <ContactIcon src={ EmailIcon } alt="email" />
       </Contact>
     </ContactBox>
   );
--- src/components/Contacts/styles.js
+++ src/components/Contacts/styles.js
@@ -2,15 +2,17 @@ import styled from 'styled-components';
 
 export const ContactBox = styled.div`
   display: flex;
-  background: ${({ theme }) => theme.colors.primary};
+  align-items: center;
+  justify-content: space-evenly;
+  margin-top: 10px;
 `;
 
 export const Contact = styled.a`
-  width: 32px;
-  background: ${({ theme }) => theme.colors.primary};
+  width: 28px;
+  line-height: 0;
+  margin: 0 5px;
 `;
 
 export const ContactIcon = styled.img`
   width: 100%;
-  background: ${({ theme }) => theme.colors.primary};
 `;
