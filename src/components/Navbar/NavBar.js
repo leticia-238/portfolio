9280cd65ce5fdb637a8fb01cdf46a2d4e2467eb4
leticia-238 <leticiaoliveiraandrade.9@gmail.com>
2022-10-13 22:15:23 -0300

    feat: adiciona tabs na navbar

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -1,11 +1,16 @@
 import { Container, Link } from './styles';
 
+const paths = {
+  home: 'home', about: 'about', projects: 'projects',
+};
+
 function NavBar() {
+  const path = 'home';
   return (
     <Container>
-      <Link href="./">link</Link>
-      <Link href="./">link</Link>
-      <Link href="./">link</Link>
+      <Link href="/home" active={ path === paths.home }>Home</Link>
+      <Link href="/about" active={ path === paths.about }>About me</Link>
+      <Link href="/projects" active={ path === paths.projects }>Projects</Link>
     </Container>
   );
 }
--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -1,13 +1,27 @@
 import styled from 'styled-components';
 
 export const Container = styled.nav`
-  background: ${(props) => props.theme.colors.secondary};
+  background: ${({ theme }) => theme.colors.bg};
   display: flex;
+  box-shadow: ${({ theme }) => theme.colors.bg_shadow};
+  padding: 5px 5px 0;
+  justify-content: center;
 `;
 
 export const Link = styled.a`
-  padding: 8px;
-  border-radius: 4px;
+  padding: 8px 10px;
+  border-radius: 8px 8px 0 0;
+  margin: 0 6px;
   text-decoration: none;
-  background: ${(props) => props.theme.colors.primary};
+  color: ${({ active, theme }) => (
+    active ? theme.colors.bg : theme.colors.primary
+  )};
+  background: ${({ active, theme }) => (
+    active ? theme.colors.primary : theme.colors.bg
+  )};
+  
+  &:hover {
+    color: ${({ theme }) => theme.colors.bg};
+    background: ${({ theme }) => theme.colors.primary};
+  }
 `;
