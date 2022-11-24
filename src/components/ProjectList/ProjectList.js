ac7c32bf03962c12c340ae0f9ce0e8b7b0e5fa2c
leticia-238 <leticiaoliveiraandrade.9@gmail.com>
2022-11-24 10:42:47 -0300

    ✨ feat: adiciona estilizações nos cards dos projetos

--- src/components/ProjectList/ProjectList.js
+++ src/components/ProjectList/ProjectList.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
-import { Card, Container, Image, Title } from './styles';
-import { PAGE_URL_BASE, GITHUB_URL_BASE } from '../../data/data';
+import { Card, CardDetails, Container, Image, Title } from './styles';
+import GitHubIconLink from '../IconLink/GitHubIconLink';
 
 function ProjectList({ projects }) {
   return (
@@ -8,9 +8,10 @@ function ProjectList({ projects }) {
       { projects.length && projects.map(({ image, title, path }, index) => (
         <Card key={ index }>
           <Image src={ image } alt={ title } />
-          <a href={ `${PAGE_URL_BASE}${path}` }>web-page</a>
-          <a href={ `${GITHUB_URL_BASE}${path}` }>github</a>
-          <Title>{ title }</Title>
+          <CardDetails>
+            <Title>{ title }</Title>
+            <GitHubIconLink path={ path } />
+          </CardDetails>
         </Card>
       ))}
     </Container>
--- src/components/ProjectList/styles.js
+++ src/components/ProjectList/styles.js
@@ -1,17 +1,38 @@
 import styled from 'styled-components';
 
 export const Title = styled.h3`
-  background: ${({ theme }) => theme.colors.primary};
+  font-size: 1rem;
+  font-weight: 500;
 `;
 
 export const Image = styled.img`
-  width: 100%;
+  background: ${({ theme }) => theme.colors.primary};
+  flex-grow: 1;
+  border-radius: inherit;
+  margin: 5px 5px 0;
 `;
 
 export const Card = styled.div`
-  background: ${({ theme }) => theme.colors.primary};
+  background: ${({ theme }) => theme.colors.terciary};
+  width: max(30%, 230px);
+  aspect-ratio: 3 / 2;
+  margin: 5px;
+  border-radius: 10px;
+  display: inline-flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const CardDetails = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 5px;
 `;
 
 export const Container = styled.div`
-background: ${({ theme }) => theme.colors.primary};
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-top: 20px;
 `;
